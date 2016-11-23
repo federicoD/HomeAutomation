56cf934decdd72f8dbc17f3474bb92c4671aecef
Federico Degrandis <degrandis.federico@gmail.com>
2016-11-23 17:28:37 +0100

    Fixed some things with the device and flask

--- app.py
+++ app.py
@@ -7,15 +7,10 @@ app = Flask(__name__)
 
 # AVR - YAMAHA HTR-4063
 
-@app.route('/avr/on', methods=['POST'])
+@app.route('/avr/power', methods=['POST'])
 def avr_on():
     avr_send_ir_code("KEY_POWER")
-    return
-
-@app.route('/avr/off', methods=['POST'])
-def avr_off():
-    avr_send_ir_code("KEY_SLEEP")
-    return
+    return ''
 
 @app.route('/avr/input/<input>', methods=['POST'])
 def avr_input(input):
@@ -29,19 +24,22 @@ def avr_input(input):
     
     formatted_input = input.uppercase()
     avr_send_ir_code("INPUT" + formatted_input)
-    return
+    return ''
 
 @app.route('/avr/volume/up', methods=['POST'])
 def avr_volume_up():
     avr_send_ir_code("KEY_VOLUMEUP")
+    return ''
 
 @app.route('/avr/volume/down', methods=['POST'])
 def avr_volume_down():
     avr_send_ir_code("KEY_VOLUMEDOWN")
+    return ''
 
 @app.route('/avr/volume/mute', methods=['POST'])
 def avr_volume_mute():
     avr_send_ir_code("VOL_MUTE")
+    return ''
 
 
 # PROJECTOR - BENQ W1070
@@ -49,12 +47,14 @@ def avr_volume_mute():
 @app.route('/projector/on', methods=['POST'])
 def projector_on():
     projector_send_ir_code("KEY_POWER")
+    return ''
 
 @app.route('/projector/off', methods=['POST'])
 def projector_off():
     projector_send_ir_code("KEY_SUSPEND")
     time.sleep(0.5)
     projector_send_ir_code("KEY_SUSPEND")
+    return ''
 
 @app.route('/projector/input/<input>', methods=['POST'])
 def projector_input(input):
@@ -64,6 +64,8 @@ def projector_input(input):
         projector_send_ir_code("HDMI-2")
     else:
         abort(400)
+    
+    return ''
 
 def avr_send_ir_code(code):
     call(["irsend", "SEND_ONCE", "YAMAHA-RAV294", code])
