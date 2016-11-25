467d104e9bd7f4add8e09cb40244c05d2a20cd88
Federico Degrandis <degrandis.federico@gmail.com>
2016-11-25 10:55:33 +0100

    Solved a couple of bugs. Still need to improve error management.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import request
 from flask import abort
-from subprocess import call
+from flask import jsonify
+import subprocess
 import time
 
 app = Flask(__name__)
@@ -21,10 +22,10 @@ def avr_input(input):
         input != "hdmi4" and
         input != "audio1" and
         input != "audio2"):
-            abort(400)
+            abort(400, 'Invalid input. Supported hdmi1/2/3/4 or audio1/2')
     
     formatted_input = input.upper()
-    avr_send_ir_code("INPUT" + formatted_input)
+    avr_send_ir_code("INPUT " + formatted_input)
     return ''
 
 @app.route('/avr/volume/up', methods=['POST'])
@@ -64,15 +65,38 @@ def projector_input(input):
     elif (input == "hdmi2"):
         projector_send_ir_code("HDMI-2")
     else:
-        abort(400)
+        abort(400, 'invalid input. Supported: hdmi1/2')
     
     return ''
 
 def avr_send_ir_code(code):
-    call(["irsend", "SEND_ONCE", "YAMAHA-RAV294", code])
+    try:
+        #call(["irsend", "SEND_ONCE", "YAMAHA-RAV294", code])
+        subprocess.check_output(["irsend", "SEND_ONCE", "YAMAHA-RAV294", code])
+    except subprocess.CalledProcessError as e:
+        abort(400, e.message)
+    except Exception as e:
+        abort(500, e.message)
 
 def projector_send_ir_code(code):
-    call(["irsend", "SEND_ONCE", "BENQ_W1070", code])
+    try:
+        #call(["irsend", "SEND_ONCE", "BENQ_W1070", code])
+        subprocess.check_output(["irsend", "SEND_ONCE", "BENQ_W1070", code])
+    except subprocess.CalledProcessError as e:
+        abort(400, e.message)
+    except Exception as e:
+        abort(500, e.message)
+
+# @app.errorhandler(400)
+# def custom400(error):
+#     response = jsonify({'message': error})
+#     return response
+
+# @app.errorhandler(500)
+# def custom500(error):
+#     response = jsonify({'message': error})
+#     return response
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
